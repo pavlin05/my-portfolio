9366eb4c6d999eac2e4dfd7d4b18126a1175361c
Pablo <>
2025-09-01 16:14:06 +0200

    feat(MP): fix i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,6 +6,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
+const userLang = navigator.language.split('-')[0]; // "it-IT" -> "it"
 
 i18n
     // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -20,12 +21,14 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
+        lng: userLang,
         fallbackLng: 'it',
+        supportedLngs: ['it', 'en', 'es'],
         debug: true,
 
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
-        }
+        },
     });
 
 
